9772ad0c410f317476d553485f9344dec593b52f
stream <penolove15@gmail.com>
2017-01-11 22:58:40 +0800

    add a only two days sqlite for git submit

--- views.py
+++ views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 import sqlite3
 import time
+from django.conf import settings
+from datetime import datetime
 
 ammount_dict={}
 
+# This tow dates should be handle properly
+# Here I reamin it as fixed
+endDate='2017.1.11'
+startDate='2017.1.10'
 
 def get_article_ammount(date):
     """function for connect to DB and get today article ammount and save it in ammount_dict"""
@@ -15,7 +21,8 @@ def get_article_ammount(date):
         else:
             return date_list[0]+'/'+date_list[1]+'/'+date_list[2]
     if(ammount_dict.get(date, None)==None):
-        conn=sqlite3.connect('/home/stream/Documents/kerkerman88/starbucks.sqlite')
+        # this require the starbucks.sqlite
+        conn=sqlite3.connect(settings.BASE_DIR+'/pttWeb/static/starbucks.sqlite')
         curs = conn.cursor()
         date_t=date_trans(date)
         get_length='SELECT COUNT(*) FROM webarticle where date = "'+date_t+'"' 
@@ -32,6 +39,7 @@ def index(request):
     return render(request, 'WebPtt/index.html')
 
 def index_if(request,id,date):
+    """return correspondes image and render by id ,date"""
     #ammount=9
     ammount=get_article_ammount(date)
     #pic_path
@@ -47,22 +55,28 @@ def index_if(request,id,date):
         P_N=1
     else:
         P_N=0
-    return render(request, 'WebPtt/Gossip_index.html',{'pic_path':path,'index_info':index_info,'P_N':P_N,'date':date,'id':id})
+    return render(request, 'WebPtt/Gossip_index.html',{'pic_path':path,'index_info':index_info,'P_N':P_N,'date':date,'id':id,'startDate':startDate,'endDate':endDate})
 
 def get_today():
+    """return today date/most recently have been parse , check if today has article"""
+    global endDate
     today=time.strftime("%m.%d")
     years=time.strftime("%Y")
     date_list=today.split(".")
     today=str(int(date_list[0]))+'.'+date_list[1]
     check_today=years+'.'+today
     print "get_today : "+ check_today
-    return check_today
-
-
+    if(get_article_ammount(check_today)>0):    
+        a=datetime.strptime(check_today,'%Y.%m.%d')-datetime.strptime(endDate,'%Y.%m.%d')
+        if(a.total_seconds()>0):
+            endDate=check_today
+        return check_today
+    else:
+       return endDate 
 
 
 def Gossip_index(request,id):
-    #date='2017.1.11'
+    """return correspondes image and render by id. check if today have data"""
     date=get_today()
     ammount=get_article_ammount(date)
 
@@ -79,4 +93,4 @@ def Gossip_index(request,id):
         P_N=1
     else:
         P_N=0
-    return render(request, 'WebPtt/Gossip_index.html',{'pic_path':path,'index_info':index_info,'P_N':P_N,'date':date,'id':id})
+    return render(request, 'WebPtt/Gossip_index.html',{'pic_path':path,'index_info':index_info,'P_N':P_N,'date':date,'id':id,'startDate':startDate,'endDate':endDate})
