40603779c7f97ba041d88f824e40daaa5762fa58
stream <penolove15@gmail.com>
2017-01-12 13:08:37 +0800

    add topic model template

--- urls.py
+++ urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^if/(?P<id>[0-9]*)$', views.Gossip_index, name='indexIF'),
     url(r'^if/(?P<id>[0-9]*)/(?P<date>.*)$', views.index_if, name='indexIF_date'),
-    url(r'^if_b/(?P<id>[0-9]*)/(?P<date>.*)$', views.index_if_back, name='indexIF_date'),
-    url(r'^if_b/(?P<id>[0-9]*)$', views.Gossip_index_back, name='indexIF'),
+    url(r'^topic$', views.Topic_index, name='topicmodel'),
+    url(r'^if_b/(?P<id>[0-9]*)/(?P<date>.*)$', views.index_if_back, name='indexIF_date_back'),
+    url(r'^if_b/(?P<id>[0-9]*)$', views.Gossip_index_back, name='indexIF_back'),
 
 ]
--- views.py
+++ views.py
@@ -4,6 +4,7 @@ import time
 from django.conf import settings
 from datetime import datetime
 import simplejson
+import glob
 
 ammount_dict={}
 url_dict={}
@@ -14,16 +15,26 @@ endDate='2017.1.11'
 startDate='2015.7.09'
 #startDate='2017.1.10'
 
+
+def date_trans_z(x):
+    """used for date transform """
+    """2017.01.09->2017/01/09 """
+    date_list=x.split('.')
+    return date_list[0]+'/'+date_list[1]+'/'+date_list[2]
+
+
 def get_article_ammount(date):
     """function for connect to DB and get today article ammount and save it in ammount_dict"""
     """if want to using different DB , check implement here"""
     def date_trans(x):
         """used for date transform """
+        """2017.1.09->2017/ 1/09 """
         date_list=x.split('.')
         if(int(date_list[1])<10):
-            return date_list[0]+'/ '+date_list[1]+'/'+date_list[2]
+            return date_list[0]+'/ '+str(int(date_list[1]))+'/'+date_list[2]
         else:
             return date_list[0]+'/'+date_list[1]+'/'+date_list[2]
+
     if(ammount_dict.get(date, None)==None):
         # this require the starbucks.sqlite
         #conn=sqlite3.connect(settings.BASE_DIR+'/pttWeb/static/starbucks.sqlite')
@@ -129,7 +140,37 @@ def Gossip_index(request,id):
 
     return render(request, 'WebPtt/Gossip_index.html',{'pic_path':path,'index_info':index_info,'P_N':P_N,'date':date,'id':id,'startDate':startDate,'endDate':endDate , 'article_url':article_url, 'url_list':url_list ,'ammount':ammount})
 
+class Week_Image(object):
+    def __init__(self,filename):
+        self.filename=filename
+        self.date=Week_Image.name2date(filename)
+    @staticmethod
+    def name2date(filename):
+        date=filename.split('/')[1].split('_')[2].replace('.png','')
+        return datetime.strptime(date,'%Y.%m.%d')
 
+def get_sorted_img_list():
+    """return path and date"""
+    dirPath=settings.BASE_DIR
+    imgdir="/pttWeb/static/topicmodel"
+    fileID=glob.glob(dirPath+imgdir+"/*.png")
+    fileID=[i.replace('/home/stream/Documents/minimum_django/pttWeb/static/','') for i in fileID]
+    fileID=[Week_Image(i) for i in fileID]
+    fileID.sort(key=lambda x: x.date, reverse=True)
+    #translate . to / since javascript parsing date has some issue!
+    fileID=[(i.filename,date_trans_z(i.date.strftime("%Y.%m.%d"))) for i in fileID]
+    return fileID
+
+def Topic_index(request):
+    """render for topic model , very simple rule"""
+    """return path,date of images of topic to javascript"""
+    fileID=get_sorted_img_list()
+    week_img_list = simplejson.dumps(fileID)
+    #latest
+    path=fileID[0][0]
+    date=fileID[0][1]
+    print date
+    return render(request, 'WebPtt/Gossip_topic_model.html',{'pic_path':path,'week_img_list':week_img_list,'date':date})
 
 def index_if(request,id,date):
     """return correspondes image and render by id ,date"""
